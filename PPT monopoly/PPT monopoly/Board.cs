b073ae00796b4c6d8b0842a6290a8d3ca849164f
Squora <87583694+Squora@users.noreply.github.com>
2022-12-29 21:13:33 +0300

    Add files via upload

---
+++ PPT monopoly/PPT monopoly/Board.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPT_monopoly
+{
+    class Board
+    {
+        private int[] board = new int[40];
+
+        private ConsoleColor startFieldColor = ConsoleColor.Red;
+        private ConsoleColor fieldColor = ConsoleColor.White;
+        private ConsoleColor maxValueColor = ConsoleColor.Yellow;
+
+        private int side = 0;
+        private int currentField = 0;
+        private int maxValue = 0;
+        private int sumOfMoves = 0;
+
+        private int lapCounter = 0;
+
+        public int getMaxValue { get { return maxValue; } }
+        public int getLapCounter { get { return lapCounter; } }
+        public int getSumOfMoves { get { return sumOfMoves; } }
+        public int[] PlayTurn(int move)
+        {
+            sumOfMoves += move;
+            currentField += move;
+            if (currentField > 39)
+            {
+                currentField -= 40;
+                lapCounter++;
+            }
+            board[currentField]++;
+            return board;
+        }
+
+        int FindMaxValue()
+        {
+            int value = -1;
+            foreach (var item in board)
+            {
+                if (value < item)
+                {
+                    value = item;
+                }
+            }
+            return value;
+        }
+
+        public void RefreshValues()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = 0;
+            }
+            side = 0;
+            maxValue = -1;
+            lapCounter = 0;
+            sumOfMoves = 0;
+        }
+
+        public void ShowBoard()
+        {
+            maxValue = FindMaxValue();
+            int rowCounter = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                switch (side)
+                {
+                    case 0:
+                        for (int j = 0; j < 11; j++)
+                        {
+                            if (j == 0)
+                            {
+                                Console.ForegroundColor = startFieldColor;
+                            }
+                            else if (board[j] == maxValue)
+                            {
+                                Console.ForegroundColor = maxValueColor;
+                            }
+                            Console.Write(board[j] + "   ");
+                            Console.ForegroundColor = fieldColor;
+                            rowCounter++;
+                            if (rowCounter > 10)
+                            {
+                                side++;
+                                rowCounter = 0;
+                                break;
+                            }
+                        }
+                        break;
+                    case 1:
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        for (int j = 11, l = 39; j < 20 && l > 30; j++, l--)
+                        {
+                            if (board[j] == maxValue)
+                            {
+                                Console.Write(board[l] + "                                       ");
+                                Console.ForegroundColor = maxValueColor;
+                                Console.Write(board[j]);
+                                Console.ForegroundColor = fieldColor;
+                                Console.WriteLine();
+                            }
+                            else if (board[l] == maxValue)
+                            {
+                                Console.ForegroundColor = maxValueColor;
+                                Console.Write(board[l] + "                                       ");
+                                Console.ForegroundColor = fieldColor;
+                                Console.Write(board[j]);
+                                Console.WriteLine();
+                            }
+                            else if (board[j] == maxValue && board[l] == maxValue)
+                            {
+                                Console.ForegroundColor = maxValueColor;
+                                Console.WriteLine(board[l] + "                                       " + board[j]);
+                                Console.ForegroundColor = fieldColor;
+                            }
+                            else
+                            {
+                                Console.WriteLine(board[l] + "                                       " + board[j]);
+                            }
+                            Console.WriteLine();
+                            rowCounter++;
+                            if (rowCounter > 8)
+                            {
+                                side++;
+                                rowCounter = 0;
+                                break;
+                            }
+                        }
+                        break;
+                    case 2:
+                        for (int j = 30; j > 19; j--)
+                        {
+                            if (board[j] == maxValue)
+                            {
+                                Console.ForegroundColor = maxValueColor;
+                            }
+                            Console.Write(board[j] + "   ");
+                            Console.ForegroundColor = fieldColor;
+                            rowCounter++;
+                            if (rowCounter > 10)
+                            {
+                                side++;
+                                rowCounter = 0;
+                                break;
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
--- PPT monopoly/PPT monopoly/Program.cs
+++ PPT monopoly/PPT monopoly/Program.cs
@@ -1,160 +1,33 @@
 ﻿using System;
+using PPT_monopoly;
 
 namespace monopoly
 {
     class Program
     {
-        static Random rnd = new Random();
+        private static Dice _dice = new Dice(6);
 
-        static int[,] playingField = new int[10, 10];
+        private static Board _board = new Board();
 
         static string userInput;
 
-        static bool isStartField = true;
-
-        static long sumOfMoves = 0;
-
-        static int side = 0;
-        static int maxValue = -1;
         static int numbersOfTakes = 0;
-        static int lapCounter = 1;
         static int numberOfRepetitions = 0;
 
         static double hitProbability = 0;
         static double averageMoves = 0;
         static double doubleDropChance = 0;
 
-        static void updatePlayingField()
-        {
-            int rowCounter = 0;
-            int columnCounter = 0;
-            foreach (var item in playingField)
-            {
-                if (isStartField == true)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    isStartField = false;
-                }
-                else if ((columnCounter == 0 || columnCounter == 9) || (rowCounter == 0 || rowCounter == 9))
-                {
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-                else if (rowCounter != 0 && rowCounter != 9 && numberOfRepetitions < 1000)
-                {
-                    Console.Write("");
-                }
-                else if (rowCounter != 0 && rowCounter != 9 && numberOfRepetitions < 9999)
-                {
-                    Console.Write(" ");
-                }
-                else if (rowCounter != 0 && rowCounter != 9 && numberOfRepetitions > 9999 && numberOfRepetitions < 100000)
-                {
-                    Console.Write("  ");
-                }
-                else if (rowCounter != 0 && rowCounter != 9 && numberOfRepetitions > 99999 && numberOfRepetitions < 1000000)
-                {
-                    Console.Write("   ");
-                }
-                else if (rowCounter != 0 && rowCounter != 9 && numberOfRepetitions > 999999)
-                {
-                    Console.Write("    ");
-                }
-                if (item == maxValue)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                }
-                Console.Write(item + "   ");
-                Console.ForegroundColor = ConsoleColor.Black;
-                columnCounter += 1;
-                if (columnCounter == 10)
-                {
-                    Console.Write('\n');
-                    Console.Write('\n');
-                    Console.Write('\n');
-                    rowCounter += 1;
-                    columnCounter = 0;
-                }
-            }
-            isStartField = true;
-        }
-
-        static int[,] makeMove(int[,] playingField)
-        {
-            int firstDue = rnd.Next(1, 7);
-            int secondDue = rnd.Next(1, 7);
-            if (firstDue == secondDue)
-            {
-                numbersOfTakes += 1;
-            }
-            int move = firstDue + secondDue;
-            sumOfMoves += move;
-            if (move > 9)
-            {
-                side += 1;
-                move -= 10;
-            }
-            if (side > 3)
-            {
-                side = 0;
-                lapCounter += 1;
-            }
-            switch (side)
-            {
-                case 0:
-                    playingField[0, move] += 1;
-                    break;
-                case 1:
-                    playingField[move, 9] += 1;
-                    break;
-                case 2:
-                    playingField[9, move] += 1;
-                    break;
-                case 3:
-                    playingField[move, 0] += 1;
-                    break;
-
-            }
-            return playingField;
-        }
-
-        static int findMaxValue(int[,] playingField)
-        {
-            int value = -1;
-            foreach (var item in playingField)
-            {
-                if (value < item)
-                {
-                    value = item;
-                }
-            }
-            return value;
-        }
-
-        static void refreshValues()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = 0; j < 10; j++)
-                {
-                    playingField[i, j] = 0;
-                }
-            }
-            isStartField = true;
-            side = 0;
-            maxValue = -1;
-            lapCounter = 1;
-            sumOfMoves = 0;
-            numbersOfTakes = 0;
-            hitProbability = 0;
-            averageMoves = 0;
-            doubleDropChance = 0;
-        }
         static void Main(string[] args)
         {
             do
             {
+                int move;
+                int dice1;
+                int dice2;
+                numbersOfTakes = 0;
                 Console.Clear();
-                refreshValues();
+                _board.RefreshValues();
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write("Введите количество повторений симуляции: ");
                 userInput = Console.ReadLine();
@@ -169,13 +42,19 @@ namespace monopoly
                 Console.Clear();
                 for (int i = 0; i < numberOfRepetitions; i++)
                 {
-                    makeMove(playingField);
+                    dice1 = _dice.Roll();
+                    dice2 = _dice.Roll();
+                    move = dice1 + dice2;
+                    if (dice1 == dice2)
+                    {
+                        numbersOfTakes++;
+                    }
+                    _board.PlayTurn(move);
                 }
-                maxValue = findMaxValue(playingField);
-                hitProbability = (double)maxValue / numberOfRepetitions;
-                averageMoves = sumOfMoves / numberOfRepetitions;
-                doubleDropChance = (double)numbersOfTakes / numberOfRepetitions;
-                updatePlayingField();
+                _board.ShowBoard();
+                hitProbability = _board.getMaxValue / (double)numberOfRepetitions;
+                averageMoves = _board.getSumOfMoves / (double)numberOfRepetitions;
+                doubleDropChance = numbersOfTakes / (double)numberOfRepetitions;
                 Console.Write('\n');
                 Console.Write('\n');
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -184,15 +63,16 @@ namespace monopoly
                 Console.WriteLine("Интерактивное поле");
                 Console.Write('\n');
                 Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Пройдено кругов: " + lapCounter);
+                Console.WriteLine($"Пройдено кругов: {_board.getLapCounter}");
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Поле на которое попадались больше всего раз: " + maxValue + " (вероятность попадания на это поле " +
-                    Math.Round(hitProbability, 2) + " или " + Math.Round(hitProbability * 100) + "%)");
+                Console.WriteLine($"Поле на которое попадались больше всего раз: {_board.getMaxValue}" +
+                    $"(вероятность попадания на это поле { Math.Round(hitProbability, 2)}" +
+                    $" или {Math.Round(hitProbability * 100)}  %)");
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("Среднее ходов: " + Math.Round(averageMoves));
+                Console.WriteLine($"Среднее ходов: {Math.Round(averageMoves)}");
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Количество дублей: " + numbersOfTakes + " (шанс выпадения дубля: " + 
-                    Math.Round(doubleDropChance, 2) + " или " + Math.Round(doubleDropChance * 100) + "%)");
+                Console.WriteLine($"Количество дублей: {numbersOfTakes} (шанс выпадения дубля: " + 
+                    $"{ Math.Round(doubleDropChance, 2)} или {Math.Round(doubleDropChance * 100)} %)");
                 Console.ReadKey();
             } while (true);
         }
